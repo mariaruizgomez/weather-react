3f8321640e832aaad4fd6bd357eddc8e352cd970
mariaruizgomez <crg.7886@gmail.com>
2023-02-13 02:42:06 -0800

    added props

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         
-        <Weather />
+        <Weather defaultCity="Las Vegas" />
         <footer>
           This project was coded by{" "}
           <a
