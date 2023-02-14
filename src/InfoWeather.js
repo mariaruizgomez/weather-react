f4eccdd5419169d30172bedab44b98e8a0a174ed
mariaruizgomez <crg.7886@gmail.com>
2023-02-13 21:55:46 -0800

    added InfoWeather component to add search engine

--- src/InfoWeather.js
+++ src/InfoWeather.js
@@ -1,41 +1,55 @@
 import React from "react";
+import FormattedDate from "./FormattedDate";
 import "./InfoWeather.css";
 
 export default function InfoWeather(props) {
   return (
     <div className="InfoWeather">
       <div className="row d-flex align-items-center">
-        <div className="col-6 col-md-4">
-          <img
-            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
-            width="200px"
-            alt=""
-          />
+        <div className="col-12 col-md-6 d-flex flex-column justify-content-center">
+          <div className="current-city">
+            <h1>{props.data.city}</h1>
+            <i className="fa-solid fa-location-dot"></i>
+          </div>
+          <div className="text-capitalize description">
+            {props.data.description}
+          </div>
         </div>
-        <div className="col-6 col-md-4">
-          <strong>12</strong>
-          <span className="units">
-            <a href="http:/" rel="noopener noreferrer">
-              °C
-            </a>{" "}
-            |
-            <a href="http:/" rel="noopener noreferrer">
-              °F
-            </a>
-          </span>
-        </div>
-        <div className="col-12 col-md-4">
-          <ul>
-            <li>February 2nd</li>
-            <hr />
+        <div className="row d-flex align-items-center">
+          <div className="col-6 col-md-4">
+            <img
+              src={props.data.iconUrl}
+              width="100px"
+              alt="{weatherData.description}"
+            />
+          </div>
+          <div className="col-6 col-md-4">
+            <strong>{Math.round(props.data.temperature)}</strong>
+            <span className="units">
+              <a href="http:/" rel="noopener noreferrer">
+                °C
+              </a>{" "}
+              |
+              <a href="http:/" rel="noopener noreferrer">
+                °F
+              </a>
+            </span>
+          </div>
+          <div className="col-12 col-md-4">
+            <ul>
+              <li>
+                <FormattedDate date={props.data.date} />
+              </li>
+              <hr />
 
-            <li>
-              Humidity: <span>5</span> %
-            </li>
-            <li>
-              Wind: <span>6</span> km/h
-            </li>
-          </ul>
+              <li>
+                Humidity: <span>{props.data.humidity}</span> %
+              </li>
+              <li>
+                Wind: <span>{props.data.wind}</span> km/h
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
-import "./Form.css";
-import FormattedDate from "./FormattedDate";
+import InfoWeather from "./InfoWeather";
 
 export default function Weather(props) {
-  const [weatherData, setweatherData] = useState({ ready: false });
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
   function handleResponse(response) {
-    setweatherData({
+    setWeatherData({
       ready: true,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
@@ -19,82 +19,48 @@ export default function Weather(props) {
       date: new Date(response.data.dt * 1000),
     });
   }
-  if (weatherData.ready) {
-    return (
-      <div className="Weather">
-        <div className="row d-flex align-items-center">
-          <div className="col-12 col-md-6 d-flex flex-column justify-content-center">
-            <div className="current-city">
-              <h1>{weatherData.city}</h1>
-              <i className="fa-solid fa-location-dot"></i>
-            </div>
-            <div className="text-capitalize description">
-              {weatherData.description}
-            </div>
-          </div>
-
-          <div className="col-12 col-md-6">
-            <form>
-              <input
-                type="search"
-                placeholder="Enter a city"
-                className="form-control"
-                autoFocus="on"
-              />
-              <div>
-                <button className="search-button" title="Search">
-                  <i className="fa-solid fa-magnifying-glass"></i>
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-
-        <div className="row d-flex align-items-center">
-          <div className="col-6 col-md-4">
-            <img
-              src={weatherData.iconUrl}
-              width="100px"
-              alt="{weatherData.description}"
-            />
-          </div>
-          <div className="col-6 col-md-4">
-            <strong>{Math.round(weatherData.temperature)}</strong>
-            <span className="units">
-              <a href="http:/" rel="noopener noreferrer">
-                °C
-              </a>{" "}
-              |
-              <a href="http:/" rel="noopener noreferrer">
-                °F
-              </a>
-            </span>
-          </div>
-          <div className="col-12 col-md-4">
-            <ul>
-              <li>
-                <FormattedDate date={weatherData.date} />
-              </li>
-              <hr />
-
-              <li>
-                Humidity: <span>{weatherData.humidity}</span> %
-              </li>
-              <li>
-                Wind: <span>{weatherData.wind}</span> km/h
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    );
-  } else {
+  function search() {
     const apiKey = "9c340174dce0d0c11428fd78107dfdf3";
     let units = "metric";
 
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${units}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
     axios.get(apiUrl).then(handleResponse);
+  }
 
-    return "Loading";
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="col-12 col-md-6">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+              autoFocus="on"
+              onChange={handleCityChange}
+            />
+            <div>
+              <button className="search-button" title="Search">
+                <i className="fa-solid fa-magnifying-glass"></i>
+              </button>
+            </div>
+          </form>
+        </div>
+
+        <InfoWeather data={weatherData} />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
   }
 }
